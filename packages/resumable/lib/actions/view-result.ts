5afe03164a9f3fc9ad9c4050d08ef15866afa23f
Ibrahim ben Salah <ibrahim.bensalah@xania.nl>
2023-02-09 02:40:56 +0100

    remove unsue script tag

--- packages/resumable/lib/actions/view-result.ts
+++ packages/resumable/lib/actions/view-result.ts
@@ -27,54 +27,7 @@ export class ViewResult implements IActionResult {
         res.write(curr.toString());
       }
     }
-    res.write(`
-    <script>
-      async function __hydrate(obj, hydrationRoot) {
-        const result = {};
 
-        const stack = [[result, 'root', obj]];
-        while(stack.length) {
-          const [t, p, src] = stack.pop();
-          if (src === null || src === undefined) {
-            t[p] = src;
-          } else if (typeof src === 'object') {
-            if (src instanceof Array) {
-              const c = [];
-              t[p] = c;
-              for(const k in src) {
-                stack.push([c, k, src[k]])
-              }
-            } else if ('__ctor' in src) {
-              const ctor = src.__ctor;
-              const instance = await import(ctor.__src).then(mod => new mod[ctor.__name]);
-              t[p] = instance;
-              for(const k in src) {
-                stack.push([instance, k, src[k]])
-              }
-            } else if ('__src' in src) {
-              t[p] = await import(src.__src).then(m => m[src.__name])
-            } else if ('__node' in src) {
-              const path = src.__node;
-              let node = hydrationRoot;
-              for(let i=0, len = path.length ; i<len ; i++) {
-                node = node.childNodes[path[i]];
-              }
-              t[p] = node;
-            } else {
-              const c = {};
-              t[p] = c;
-              for(const k in src) {
-                stack.push([c, k, src[k]])
-              }
-            }
-          } else {
-            t[p] = src;
-          }
-        }
-        return result.root;
-      }
-    </script>
-    `);
     res.end();
   }
 }
